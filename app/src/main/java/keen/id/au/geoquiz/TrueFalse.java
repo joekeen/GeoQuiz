66cb11d51e44ab5e4c7b5b5ded3d3483d24afe03
Joe Keen <joe@keen.id.au>
2015-03-31 22:07:34 +1100

    Chapter 2. Add support for multiple questions.

---
+++ app/src/main/java/keen/id/au/geoquiz/TrueFalse.java
@@ -0,0 +1,30 @@
+package keen.id.au.geoquiz;
+
+/**
+ * Created by joe on 31/03/15.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int mQuestion, boolean mTrueQuestion) {
+        this.mQuestion = mQuestion;
+        this.mTrueQuestion = mTrueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
